9a63053fb7f71a1b19df875e20e4d6ea60575129
Mori-Atsushi <e1237@s.akashi.ac.jp>
2017-01-06 14:09:19 +0900

    課題7 ニュートンの3次前進差分補間

--- e1237_07.c
+++ e1237_07.c
@@ -14,9 +14,9 @@ double liner(double x) {
     return (1 - alpha) * function(a) + alpha * function(b);
 }
 
-/* ニュートンの2次前進差分補間(1) */
+/* ニュートンの2次前進差分補間 */
 double newton1(double x) {
-    double a[3], b[3], r, ans, temp, delta;
+    double a[3], b[3], r;
     int i, j;
 
     a[0] = ((int)(x * 10)) / 10.0;
@@ -30,6 +30,27 @@ double newton1(double x) {
     return b[0] + r * (b[1] - b[0]) + r * (r - 1) / 2 * ((b[2] - b[1]) - (b[1] - b[0]));
 }
 
+/* ニュートンの3次前進差分補間 */
+double newton2(double x) {
+    double a[4], b[4], r, ans;
+    int i, j;
+
+    a[0] = ((int)(x * 10)) / 10.0;
+    for(i = 1; i < 4; i++)
+        a[i] = a[i - 1] + 0.1;
+    r = (x - a[0]) / 0.1;
+    
+    for(i = 0; i < 4; i++)
+        b[i] = function(a[i]);
+
+    ans = b[0];
+    ans += r * (b[1] - b[0]);
+    ans += r * (r - 1) / 2 * ((b[2] - b[1]) - (b[1] - b[0]));
+    ans += (r * (r - 1) * (r - 2)) / (3 * 2) * (((b[3] - b[2]) - (b[2] - b[1])) - ((b[2] - b[1]) - (b[1] - b[0])));
+
+    return  ans;
+}
+
 int main(void) {
     const double ans = function(TARGET);
     double y;
@@ -40,6 +61,10 @@ int main(void) {
 
     printf("[Newton polynomial(1)]\n");
     y = newton1(TARGET);
+    printf("|%lf - %lf| = %lf\n\n", ans, y, fabs(ans - y));
+
+    printf("[Newton polynomial(2)]\n");
+    y = newton2(TARGET);
     printf("|%lf - %lf| = %lf\n\n", ans, y, fabs(ans - y));
 
     return 0;
