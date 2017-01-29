26b23fd17a967811e3bff0d69b17e7b8a8cb3e2b
Mori-Atsushi <e1237@s.akashi.ac.jp>
2017-01-29 16:41:12 +0900

    課題9 後退差分近似完了

--- e1237_09.c
+++ e1237_09.c
@@ -14,6 +14,16 @@ double forwardDifference2(int target, double f[5]) {
     return f[target + 2] - 2 * f[target + 1] + f[target];
 }
 
+//後退差分近似 1階微分係数
+double backwardDifference1(int target, double f[5]) {
+    return f[target] - f[target - 1];
+}
+
+//後退差分近似 2階微分係数
+double backwardDifference2(int target, double f[5]) {
+    return f[target] - 2 * f[target - 1] + f[target - 2];
+}
+
 int main(void) {
     int i;
     double f[5];
@@ -25,6 +35,11 @@ int main(void) {
     printf("[forward difference]\n");
     printf("first   :%lf\n", forwardDifference1(TARGET, f));
     printf("second  :%lf\n", forwardDifference2(TARGET, f));
+
+    //後退差分近似
+    printf("[backward difference]\n");
+    printf("first   :%lf\n", backwardDifference1(TARGET, f));
+    printf("second  :%lf\n", backwardDifference2(TARGET, f));
 
     return 0;
 }
