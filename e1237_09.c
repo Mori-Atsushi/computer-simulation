a2ced4542eb040578e7fa50847144a52605ddbb9
Mori-Atsushi <e1237@s.akashi.ac.jp>
2017-01-29 16:44:38 +0900

    課題9 中間差分近似完了

--- e1237_09.c
+++ e1237_09.c
@@ -24,6 +24,16 @@ double backwardDifference2(int target, double f[5]) {
     return f[target] - 2 * f[target - 1] + f[target - 2];
 }
 
+//中間差分近似 1階微分係数
+double intermediateDifference1(int target, double f[5]) {
+    return (f[target + 1] - f[target - 1]) / 2;
+}
+
+//中間差分近似 2階微分係数
+double intermediateDifference2(int target, double f[5]) {
+    return f[target + 1] - 2 * f[target] + f[target - 1];
+}
+
 int main(void) {
     int i;
     double f[5];
@@ -40,6 +50,11 @@ int main(void) {
     printf("[backward difference]\n");
     printf("first   :%lf\n", backwardDifference1(TARGET, f));
     printf("second  :%lf\n", backwardDifference2(TARGET, f));
+
+    //中間差分近似
+    printf("[intermediate difference]\n");
+    printf("first   :%lf\n", intermediateDifference1(TARGET, f));
+    printf("second  :%lf\n", intermediateDifference2(TARGET, f));
 
     return 0;
 }
